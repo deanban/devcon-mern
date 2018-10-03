1e588165ec4ef1d556aa1a862383005ed3d31e0e
deanban <pbp2005@gmail.com>
2018-10-03 13:58:34 -0400

    register, login validation done. Profile progress.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,11 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Profile = require(".././models/Profile");
+const User = require(".././models/User");
+
 //@route GET api/profile/test
 //@dsc tests profile route
 //@access public
 router.get("/test", (req, res) =>
   res.json({ msg: "hello world from profile" })
 );
+
+//@route GET api/profile
+//@dsc gets current user profile
+//@access private
 
 module.exports = router;
