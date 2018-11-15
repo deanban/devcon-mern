1952c06ed1e892977621774c505c523d286dc7e8
deanban <pbp2005@gmail.com>
2018-11-15 14:51:37 -0500

    footer changed to sticky footer at the bottom

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+//sticky footer
 export default function Footer() {
   return (
-    <div>
+    <div className="footer bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} Developer Connector
+    </div>
+  );
+}
+
+{
+  /*<div>
       <footer className="bg-dark text-white mt-5 p-4 text-center">
         Copyright &copy; {new Date().getFullYear()} Developer Connector
       </footer>
-    </div>
-  );
+    </div>*/
 }
