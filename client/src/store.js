e4c3dd885f3d048575a8722ce4704f9bcbe3f956
deanban <pbp2005@gmail.com>
2018-11-17 00:50:24 -0500

    devtool disabled

--- client/src/store.js
+++ client/src/store.js
@@ -10,7 +10,7 @@ const store = createStore(
   state,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
