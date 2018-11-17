203cde1470e19adbfaf7264ea5db1af3adf95d36
deanban <pbp2005@gmail.com>
2018-11-16 22:56:00 -0500

    updated texts

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} Developer Connector
+      Copyright &copy; {new Date().getFullYear()} Developer Network
     </div>
   );
 }
