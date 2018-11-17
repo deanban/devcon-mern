9736d7c90dca49866a024f80d75f17363bb3310c
deanban <pbp2005@gmail.com>
2018-11-17 12:16:46 -0500

    fixing the footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,8 +3,10 @@ import React from "react";
 //sticky footer
 export default function Footer() {
   return (
-    <div className="footer bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} Developer Network
-    </div>
+
+      <div className="footer bg-dark text-white mt-5 p-4 text-center">
+        Copyright &copy; {new Date().getFullYear()} Developer Network
+      </div>
+
   );
 }
